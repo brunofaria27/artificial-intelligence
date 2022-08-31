c06a97d17012ddfe5cdbebb1c321e791374a5bb6
Bruno Rodrigues Faria <bruno.fariadevl@gmail.com>
2022-08-31 15:00:52 -0300

    Consertando a Decision Tree - Dados binarios e não binarios

---
+++ Decision Tree/decisiontreerestaurant.py
@@ -0,0 +1,80 @@
+# Importando bibliotecas
+import pandas as pd
+from sklearn import tree
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+
+import pydot
+import pydotplus
+
+# Importando bibliotecas para calcular métricas
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+# Importar a biblioteca para mostrar a matriz de confusão da árvore
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+from IPython.display import Image
+
+# Ler dados do DataSet
+csv_dir_treinamento = "/content/sample_data/geral.csv"
+data_treino = pd.read_csv(csv_dir_treinamento, delimiter=";")
+
+# Tratar dados nominais para númericos (Treinamento)
+treinamento_classification = data_treino['conc']
+data_treino.drop(["conc"], axis=1, inplace=True)
+data_treino.drop(["Exemplo"], axis=1, inplace=True)
+treinamento_dados = pd.get_dummies(data_treino, columns=['Cliente', 'Preço', 'Tipo', 'Tempo'])
+
+# Atualizar tabela com binários
+labelencoder =  preprocessing.LabelEncoder()
+treinamento_dados['Alternativo'] = labelencoder.fit_transform(treinamento_dados['Alternativo'])
+treinamento_dados['Bar'] = labelencoder.fit_transform(treinamento_dados['Bar'])
+treinamento_dados['Sex/Sab'] = labelencoder.fit_transform(treinamento_dados['Sex/Sab'])
+treinamento_dados['fome'] = labelencoder.fit_transform(treinamento_dados['fome'])
+treinamento_dados['Chuva'] = labelencoder.fit_transform(treinamento_dados['Chuva'])
+treinamento_dados['Res'] = labelencoder.fit_transform(treinamento_dados['Res'])
+
+# Separar tabelas necessárias
+dataset_teste = treinamento_dados.loc[8:]
+dataset_teste_class = treinamento_classification.loc[8:]
+dataset_treino = treinamento_dados.loc[0:7]
+dataset_treino_class = treinamento_classification.loc[0:7]
+
+# Criando a árvore e definindo o criterio de criação usando entropia
+tree_restaurant = DecisionTreeClassifier(criterion="entropy")
+tree_restaurant = tree_restaurant.fit(dataset_treino, dataset_treino_class)
+
+# Printar a árvore
+dot_data = tree.export_graphviz(tree_restaurant, out_file=None,
+                                feature_names=dataset_treino.columns.values,
+                                class_names=treinamento_classification.unique(),
+                                filled=True, rounded=True,
+                                special_characters=True)
+graph = pydot.graph_from_dot_data(dot_data)
+graph[0].write_png('tree_restaurant.png')
+Image(filename='tree_restaurant.png')
+
+# Gerar e mostrar matriz de confusão
+conf_matrix = confusion_matrix(dataset_teste_class, tree_restaurant.predict(dataset_teste))
+labels = ["Vai esperar", "Não vai esperar"]
+cmd = ConfusionMatrixDisplay(conf_matrix, display_labels=labels)
+cmd.plot(values_format="d")
+plt.show()
+
+# Mostrar métricas
+print("Accuracy score = ", accuracy_score(dataset_teste_class, tree_restaurant.predict(dataset_teste)))
+print('\n')
+print(classification_report(dataset_teste_class, tree_restaurant.predict(dataset_teste), target_names=labels))
+print('\n')
+tn, fp, fn, tp = confusion_matrix(dataset_teste_class, tree_restaurant.predict(dataset_teste)).ravel()
+tnr = tn / (tn + fp) # true negative rate 
+fpr = fp / (tn + fp) # false positive rate 
+fnr = fn / (fn + tp) # false negative rate 
+tpr = tp / (tp + fn) # true positive rate
+print('True negative rate: ' + str(tnr))
+print('False positive rate: ' + str(fpr))
+print('False negative rate: ' + str(fnr))
+print('True positive rate: ' + str(tpr))
---
+++ Decision Tree/decisiontreeweather.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# Importando bibliotecas
+import pandas as pd
+from sklearn import tree
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+
+import pydot
+import pydotplus
+
+from IPython.display import Image
+
+# Importar a biblioteca para mostrar a matriz de confusão da árvore
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+# Importando bibliotecas para calcular métricas
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+# Ler dados do DataSet
+csv_dir_treinamento = "/content/sample_data/weather.nominal.csv"
+data_treino = pd.read_csv(csv_dir_treinamento, delimiter=",")
+
+# Tratar dados nominais para númericos (Treinamento)
+treinamento_classification = data_treino['play']
+data_treino.drop(["play"], axis=1, inplace=True)
+treinamento_dados = pd.get_dummies(data_treino, columns=['outlook', 'temperature', 'humidity'])
+
+# Atualizar tabela com binários
+labelencoder =  preprocessing.LabelEncoder()
+treinamento_dados['windy'] = labelencoder.fit_transform(treinamento_dados['windy'])
+
+# Fazer Slice nas tabelas de forma a separar treino e teste
+dataset_teste = treinamento_dados.loc[10:]
+dataset_teste_class = treinamento_classification.loc[10:]
+dataset_treino = treinamento_dados.loc[0:9]
+dataset_treino_class = treinamento_classification.loc[0:9]
+
+# Criando a árvore e definindo o criterio de criação usando entropia
+tree_weather = DecisionTreeClassifier(criterion="entropy")
+tree_weather = tree_weather.fit(dataset_treino, dataset_treino_class)
+
+dot_data = tree.export_graphviz(tree_weather, out_file=None,
+                                feature_names=dataset_treino.columns.values,
+                                class_names=treinamento_classification.unique(),
+                                filled=True, rounded=True,
+                                special_characters=True)
+graph = pydot.graph_from_dot_data(dot_data)
+graph[0].write_png('tree_weather.png')
+Image(filename='tree_weather.png')
+
+# Mostrar a matriz de confusão
+conf_matrix = confusion_matrix(dataset_teste_class, tree_weather.predict(dataset_teste))
+labels = ["Vai jogar", "Não vai jogar"]
+cmd = ConfusionMatrixDisplay(conf_matrix, display_labels=labels)
+cmd.plot(values_format="d")
+plt.show()
+
+# Mostrar métricas
+print("Accuracy score = ", accuracy_score(dataset_teste_class, tree_weather.predict(dataset_teste)))
+print('\n')
+print(classification_report(dataset_teste_class, tree_weather.predict(dataset_teste), target_names=labels))
+print('\n')
+tn, fp, fn, tp = confusion_matrix(dataset_teste_class, tree_weather.predict(dataset_teste)).ravel()
+tnr = tn / (tn + fp) # true negative rate 
+fpr = fp / (tn + fp) # false positive rate 
+fnr = fn / (fn + tp) # false negative rate 
+tpr = tp / (tp + fn) # true positive rate
+print('True negative rate: ' + str(tnr))
+print('False positive rate: ' + str(fpr))
+print('False negative rate: ' + str(fnr))
+print('True positive rate: ' + str(tpr))
